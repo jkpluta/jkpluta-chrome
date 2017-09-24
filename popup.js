ee8fb112970eff48b220ee4f645027c60f6d252c
Jan K. Pluta <jkpluta@gmail.com>
2017-09-24 11:35:55 +0200

    Ikony

---
+++ popup.js
@@ -0,0 +1,23 @@
+// $('body').style('min-width', (9 * window.width / 10).toString() + 'px');
+// $('body').style('width', '300px');
+
+$(document).ready(function() {
+  $.ajax({
+    url: 'https://jkpluta.github.io/icons.html',
+    cache: false,
+    success: function (html) {
+      var links = $('a[icon], a[icon_uri]', html);
+      $('#container').append('<p>');
+      for (var i = 0; i < links.length; i++) {
+          var link = links.eq(i);
+          if (link.attr('ICON_URI') != null)
+              $('#container').append('<a href="' + link.attr('href') + '"><img src="' + link.attr('ICON_URI') + '" alt="' + link.text() + '" title="' + link.text() + '" width="24" height="24"></a> ');
+      }
+      $('#container').append('</p>');
+      $('#container').find('a').attr('target', '_blank');
+    },
+    error: function (xhr, status, error) {
+      $('#container').append('BŁĄD!');
+    }
+  });
+})
